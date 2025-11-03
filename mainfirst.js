094200632982ea76dad8edc560a288ccc66cc3f8
Alessandro Aronica <ale.aronica@outlook.it>
2025-11-03 23:26:35 +0100

    chiamata a striveschool-api

--- mainfirst.js
+++ mainfirst.js
@@ -12,3 +12,19 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+const searchInput = "queen"
+
+const getData = async () => {
+
+  const URLData = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`
+
+  const response = await fetch(URLData)
+
+  return response.json()
+
+}
+
+getData()
+  .then(res => console.log(res))
